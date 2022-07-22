f754df6fd4db961c6ca92291f5bf28348b64a0e7
Kosuke2000 <youuchiharu@gmail.com>
2022-07-23 08:36:37 +0900

    feat: 😙雛形を作成

---
+++ src/index.ts
@@ -0,0 +1,83 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+}
+
+addEventListener("fetch", (event) => {
+  const data = [
+    {
+      id: "0",
+      name: "kunimi",
+      party: [
+        "steelix",
+        "salamander",
+        "heatran",
+        "coil",
+        "electabuzz",
+        "roserade",
+      ],
+    },
+    {
+      id: "0",
+      name: "inoue",
+      party: [
+        "steelix",
+        "salamander",
+        "heatran",
+        "coil",
+        "electabuzz",
+        "roserade",
+      ],
+    },
+    {
+      id: "0",
+      name: "hirama",
+      party: [
+        "steelix",
+        "salamander",
+        "heatran",
+        "coil",
+        "electabuzz",
+        "roserade",
+      ],
+    },
+    {
+      id: "0",
+      name: "hoi",
+      party: [
+        "steelix",
+        "salamander",
+        "heatran",
+        "coil",
+        "electabuzz",
+        "roserade",
+      ],
+    },
+  ];
+
+  const json = JSON.stringify(data);
+
+  return event.respondWith(
+    new Response(json, {
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    })
+  );
+});
